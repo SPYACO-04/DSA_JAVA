a2277a7cd81e5d6dfd932c842fbb7fbd141f9d58
Suraj Panda <surajpanda5474@gmail.com>
2026-08-12 16:31:25 +0530

    Add files via upload

---
+++ BinarySearch2D/Search.java
@@ -0,0 +1,21 @@
+package BinarySearch2D;
+
+public class Search {
+    static void main(String[] args) {
+
+    }
+    static int searchin2D(int[] arr, int target0){
+        int row = arr.length;
+        int col = arr[0].length();
+        int start = 0;
+        int end = row * col - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start) / 2;
+
+        }
+
+
+    }
+}
