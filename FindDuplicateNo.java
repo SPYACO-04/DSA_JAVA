99c0f7bea884945a82d4682a3b9b0b2fa9359242
Suraj Panda <surajpanda5474@gmail.com>
2026-08-16 12:51:46 +0530

    Add files via upload

---
+++ FindDuplicateNo.java
@@ -0,0 +1,26 @@
+package Sorting;
+//Q.287
+public class FindDuplicateNo {
+    static void main(String[] args) {
+
+    }
+
+    static int sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1){
+                int correct = nums[i] - 1;
+                 if (nums[i] != nums[correct]) {
+                    int temp = nums[i];
+                    nums[i] = nums[correct];
+                    nums[correct] = temp;
+                } else{
+                    return nums[i];
+                }
+            }else{
+                i++;
+            }
+        }
+        return -1;
+    }
+}
---
+++ FindDuplicatesArray.java
@@ -0,0 +1,31 @@
+package Sorting;
+//Q.442
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDuplicatesArray {
+    static void main(String[] args) {
+
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else{
+                i ++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < nums.length; index ++){
+            if(nums[index] != index + 1){
+                ans.add(nums[index]);
+            }
+        }
+        return ans;
+    }
+}
