10ba7488116a24fa665cda229aa3c3f5cac56c1b
Suraj Panda <surajpanda5474@gmail.com>
2026-08-23 16:04:02 +0530

    Add files via upload

---
+++ Strings/Output.java
@@ -0,0 +1,13 @@
+package Strings;
+import java.util.Arrays;
+
+public class Output {
+    static void main(String[] args) {
+        System.out.println(234);
+        System.out.println("Suraj");
+        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+
+        String a = null;
+        System.out.println(a);
+    }
+}
