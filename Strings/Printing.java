efdaea96578c1f3ee3474c49df7886a7c21fecb8
Suraj Panda <surajpanda5474@gmail.com>
2026-08-27 09:17:46 +0530

    Add files via upload

---
+++ Strings/Printing.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class Printing {
+    static void main(String[] args) {
+        float a= 213.321f;
+        System.out.printf("formated no is %.2f", a);
+
+        System.out.printf("\nPie : %.3f", Math.PI);
+
+        System.out.printf("\nHello my  name is %s and i am %s ", "Suraj", "Cool");
+
+
+    }
+}
